a7cdeb92d87b6e1a19243a4e21539a10146f66b3
Sam Morgan <s_morgan@me.com>
2017-11-13 18:09:22 +0000

    Refactor to more OO

---
+++ src/pull-request.js
@@ -0,0 +1,21 @@
+const PullRequest = function(pullRequestData) {
+  this.githubUsername = pullRequestData.user.login
+  this.url = `${pullRequestData.html_url}/files?diff=unified`
+
+  this.toPdf = async (instance, page) => {
+    console.log('Opening with PhantomJS', this.url)
+    await page.open(this.url)
+
+    console.log('Rendering PDF for', this.githubUsername)
+    await page.render(`./scans/${this._filename()}`)
+
+    console.log('All done! Check ./scans for the PDF scans.')
+    await instance.exit()
+  }
+
+  this._filename = () => {
+    return `${this.githubUsername}.pdf`
+  }
+}
+
+module.exports = PullRequest
